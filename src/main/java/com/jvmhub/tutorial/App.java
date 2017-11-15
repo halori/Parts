0d5e588c110c0c9baae0c703bc7eb1fd08d7995f
oliver <oliver@tomate.gendut.dyndns.org>
2017-11-15 23:53:36 +0100

    test data GENERATOR

--- src/main/java/com/jvmhub/tutorial/App.java
+++
@@ -1,8 +0,0 @@
-package com.jvmhub.tutorial;
-
-
-public class App {
-	public static void main(String[] args) {
-
-	}
-}
--- src/test/java/de/ogli/parts/TestDb.java
+++ src/test/java/de/ogli/parts/TestDb.java
@@ -2,34 +2,26 @@ package de.ogli.parts;
 
 import junit.framework.TestCase;
 
-import java.awt.font.TransformAttribute;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-import de.ogli.parts.entities.Component;
-import de.ogli.parts.entities.Transformation;
-import de.ogli.parts.entities.TransformationId;
+import de.ogli.parts.utils.TestDataGenerator;
 
 public class TestDb extends TestCase {
 
+	long N = 50000000;
+	
 	public void testDb() {
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-
-		Component c1 = new Component("Test1");
-		session.save(c1);
-
-		Component c2 = new Component("Test1");
-		session.save(c2);
+		int batchSize = 5000;
 		
-		Transformation t = new Transformation(new TransformationId(c1.getId(), c2.getId()), "Rotate(x,y,z,degree).Move(u,v,w)");
-
-		session.save(t);
-		
-		session.getTransaction().commit();
+		for(int i = 0; i < N; i += batchSize) {
+			System.out.println("created "+i+" from "+N+" components");
+	    	TestDataGenerator tdg = new TestDataGenerator(session, batchSize);
+	    	tdg.createBatch();
+	    }
 		session.close();
 	}
 }
