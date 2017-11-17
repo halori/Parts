7f31abf23e3b09dbfa822bc4a273bbbc64a3b35d
oliver <oliver@tomate>
2017-11-17 23:54:05 +0100

    recursive sql loader

--- src/main/java/de/ogli/parts/dbaccess/ComponentGraphLoaderBySqlRecursion.java
+++ src/main/java/de/ogli/parts/dbaccess/ComponentGraphLoaderBySqlRecursion.java
@@ -1,46 +1,52 @@
 package de.ogli.parts.dbaccess;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
-import org.hibernate.Query;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 
 import de.ogli.parts.entities.Component;
 import de.ogli.parts.entities.Transformation;
 
-public class ComponentGraphLoaderBySqlRecursion implements ComponentGraphLoader
-{
+public class ComponentGraphLoaderBySqlRecursion implements ComponentGraphLoader {
 
 	public String shortName() {
 		return "SQL-Recursion";
 	}
+
 	public ComponentGraph loadComponentGraph(Session session, long componentId) {
-		HashMap<Long, Component> componentsById = new HashMap<Long, Component>();
-		HashSet<Transformation> transformations = new HashSet<Transformation>();
-        loadRecursive(session, componentId, componentsById, transformations);
 		HashSet<Component> components = new HashSet<Component>();
-		components.addAll(componentsById.values());
-		return new ComponentGraph(components , transformations);
-	}
-	
-	private void loadRecursive(Session session, 
-			long componentId, HashMap<Long, Component> componentsById,
-			HashSet<Transformation> transformations) {
+		SQLQuery queryComponents = session.createSQLQuery(
+				"WITH RECURSIVE parts(id) AS (" + 
+				"    SELECT id FROM component where id = "+componentId+ 
+				"  UNION" + 
+				"    SELECT t.child_id" + 
+				"    FROM transformation t, parts p WHERE p.id = t.parent_id" + 
+				"  )" + 
+				"SELECT * FROM component c WHERE EXISTS (SELECT * FROM parts p WHERE p.id = c.id) ");
 		
-		if (componentsById.containsKey(componentId))
-				return;
-		Component c = (Component) session.load(Component.class, componentId);
-		componentsById.put(componentId, c);
-		Query queryDirectTransfomations = session.createQuery("from Transformation t where t.parentComponentId = :cid)");
-        queryDirectTransfomations.setParameter("cid", c.getId());
+		queryComponents.addEntity(Component.class);
 		@SuppressWarnings("unchecked")
-		List<Transformation>  directTransformations = (List<Transformation>) queryDirectTransfomations.list();
-        for (Transformation t : directTransformations) {
-        	transformations.add(t);
-        	loadRecursive(session, t.getChildComponentId(), componentsById, transformations);
-        	
-        }
+		List<Component> allComponents = (List<Component>) queryComponents.list();
+        components.addAll(allComponents);
+        
+        
+        HashSet<Transformation> transformations = new HashSet<Transformation>();
+        SQLQuery queryTransformations = session.createSQLQuery(
+				"WITH RECURSIVE parts(id) AS (" + 
+				"    SELECT id FROM component where id = "+componentId+ 
+				"  UNION" + 
+				"    SELECT t.child_id" + 
+				"    FROM transformation t, parts p WHERE p.id = t.parent_id" + 
+				"  )" + 
+				"SELECT * FROM transformation t WHERE EXISTS (SELECT * FROM parts p WHERE p.id = t.parent_id) ");
+		
+		queryTransformations.addEntity(Transformation.class);
+		@SuppressWarnings("unchecked")
+		List<Transformation>  allTransformations = (List<Transformation>) queryTransformations.list();
+        transformations.addAll(allTransformations);
+
+		return new ComponentGraph(components, transformations);
 	}
 }
